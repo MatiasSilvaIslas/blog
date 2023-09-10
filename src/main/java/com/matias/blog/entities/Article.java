4f24901d748634e268a447c6c18364b12fe18f5a
MatiasSilvaIslas <matias.silva@alumnos.frgp.utn.edu.ar>
2023-09-10 19:18:44 -0300

    Inicialización del proyecto

---
+++ src/main/java/com/matias/blog/entities/Article.java
@@ -0,0 +1,27 @@
+package com.matias.blog.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "article", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "content", nullable = false)
+    private String content;
+}
---
+++ src/test/java/com/matias/blog/BlogApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
